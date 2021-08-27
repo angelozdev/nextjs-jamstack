4da35c892961ea55c3ebb9bd698548b0564c6cd5
Angelo Zambrano <angelozam17@gmail.com>
2021-08-26 20:24:33 -0500

    :sparkles: feat: add internationalization

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,21 +1,59 @@
 import NextLink from "next/link";
 import { Routes } from "@constants";
-import { Heading, TreeIcon, Pane, Link } from "evergreen-ui";
+import { Heading, TreeIcon, Pane, Link, Combobox } from "evergreen-ui";
 import { Wrapper } from "@components";
+import { useRouter } from "next/router";
+import { useCallback } from "react";
 
 function Header() {
+  const router = useRouter();
+  const { locale, locales } = router;
+
+  const handleOnChangeLocale = useCallback((value: string) => {
+    window
+      .fetch(`/api/language`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ locale: value }),
+      })
+      .catch(console.error)
+      .finally(() => window.location.replace("/"));
+  }, []);
+
+  if (!locales || !locale) {
+    return null;
+  }
+
   return (
     <Pane is="header" elevation={1}>
       <Wrapper maxWidth="1280px">
-        <Pane paddingY=".5rem">
-          <NextLink href={Routes.HOME} passHref>
-            <Link paddingY="1rem" display="inline-block" cursor="pointer">
-              <Heading is="h1" display="flex" gap=".5rem">
-                <TreeIcon color="green500" />
-                Treepedia
-              </Heading>
-            </Link>
-          </NextLink>
+        <Pane
+          paddingY=".5rem"
+          display="flex"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Pane>
+            <NextLink href={Routes.HOME} passHref>
+              <Link paddingY="1rem" display="inline-block" cursor="pointer">
+                <Heading is="h1" display="flex" gap=".5rem">
+                  <TreeIcon color="green500" />
+                  Treepedia
+                </Heading>
+              </Link>
+            </NextLink>
+          </Pane>
+
+          {!!locales.length && (
+            <Pane>
+              <Combobox
+                initialSelectedItem={locale}
+                items={locales}
+                width={120}
+                onChange={handleOnChangeLocale}
+              />
+            </Pane>
+          )}
         </Pane>
       </Wrapper>
     </Pane>
--- pages/api/preview/clear.ts
+++ pages/api/preview/clear.ts
@@ -3,12 +3,12 @@ import { NextApiRequest, NextApiResponse } from "next";
 function clearPreview(req: NextApiRequest, res: NextApiResponse) {
   const callback = req.query?.callback;
   res.clearPreviewData();
-
   if (typeof callback !== "string") {
     return res.redirect("/");
   }
 
   res.redirect(callback);
+  res.end();
 }
 
 export default clearPreview;
